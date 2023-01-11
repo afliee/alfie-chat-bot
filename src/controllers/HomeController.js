2a235a1880ea90fa71320e25576b888a16b9345b
Kunzzz <quoctruong310803@gmail.com>
2023-01-11 18:03:23 +0700

    change function postWebhook

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
 };
@@ -7,8 +11,13 @@ let postWebhook = (req, res) => {
     let body = req.body;
     if (body.object === 'page') {
         body.entry.forEach((entry) => {
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
         res.status(200).send('EVENT_RECEIVED');
     } else {
@@ -16,7 +25,6 @@ let postWebhook = (req, res) => {
     }
 };
 let getWebhook = (req, res) => {
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
@@ -34,6 +42,15 @@ let getWebhook = (req, res) => {
         }
     }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
 module.exports = {
     getHomePage: getHomePage,
     postWebhook: postWebhook,
