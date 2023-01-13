e6e61447bfafecfcd3b795edbeb437b06a871de2
Kunzzz <quoctruong310803@gmail.com>
2023-01-13 17:52:44 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import request from 'request';
+import chatService from '../service/chatService';
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
@@ -113,7 +114,7 @@ function handleMessage(sender_psid, received_message) {
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+async function handlePostback(sender_psid, received_postback) {
     console.log('in postback');
     let response;
 
@@ -121,18 +122,18 @@ function handlePostback(sender_psid, received_postback) {
     let payload = received_postback.payload;
 
     // Set the response based on the postback payload
-    // if (payload === 'yes') {
-    //     response = { text: 'Thanks!' };
-    // } else if (payload === 'no') {
-    //     response = { text: 'Oops, try sending another image.' };
-    // }
     switch (payload) {
         case 'yes':
             response = { text: 'Thanks!' };
+            break;
         case 'no':
             response = { text: 'Oops, try sending another image.' };
+            break;
         case 'GET_STARTED':
-            response = { text: "Hello!, Welcome to the Kunz's Application" };
+            await chatService.handleGetStarted(sender_psid);
+            break;
+        default:
+            response = { text: 'Oops, What did you do?' };
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
---
+++ src/service/chatService.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+import request from 'request';
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        recipient: {
+            id: sender_psid,
+        },
+        message: response,
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request(
+        {
+            uri: 'https://graph.facebook.com/v9.0/me/messages',
+            qs: { access_token: PAGE_ACCESS_TOKEN },
+            method: 'POST',
+            json: request_body,
+        },
+        (err, res, body) => {
+            if (!err) {
+                console.log('message sent!');
+            } else {
+                console.error('Unable to send message:' + err);
+            }
+        }
+    );
+}
+const handleGetStarted = (sender_psid) => {
+    return Promise(async (resolve, reject) => {
+        try {
+            response = { text: "Hello!, Welcome to the Kunz's Application" };
+            await this.callSendAPI(sender_psid, response);
+            resolve('done');
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+module.export = {
+    handleGetStarted: handleGetStarted,
+};
