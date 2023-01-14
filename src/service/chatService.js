2ae358f5bf059c892d5c91011af684f450dd5f55
Kunzzz <quoctruong310803@gmail.com>
2023-01-14 15:22:32 +0700

    update

--- src/service/chatService.js
+++ src/service/chatService.js
@@ -29,11 +29,36 @@ function callSendAPI(sender_psid, response) {
         }
     );
 }
+
+let getUserName = (sender_psid) => {
+    // Send the HTTP request to the Messenger Platform
+    return new Promise((resolve, reject) => {
+        request(
+            {
+                uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+                method: 'GET',
+            },
+            (err, res, body) => {
+                console.log(body);
+                if (!err) {
+                    body = JSON.parse(body);
+                    let userName = `${body.first_name} ${body.last_name}`;
+                    console.log('message sent!');
+                    resolve(userName);
+                } else {
+                    console.error('Unable to send message:' + err);
+                    reject(err);
+                }
+            }
+        );
+    });
+};
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
+            let userName = await getUserName(sender_psid);
             let response = {
-                text: "Hello!, Welcome to the Kunz's Application",
+                text: `Hello!, Welcome ${userName} to the Kunz's Application`,
             };
             await callSendAPI(sender_psid, response);
             resolve('done');
